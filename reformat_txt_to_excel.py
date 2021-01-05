040c9c2c74c18b0abd0524fcab6968194e83b24e
Darin Korabov <korabovdarin@gmail.com>
2021-01-06 00:47:58 +0200

    Added description

---
+++ reformat_txt_to_excel.py
@@ -0,0 +1,28 @@
+"""
+    This script is used to convert the txt file to an Excel sheet
+    and a csv file.
+"""
+
+import pandas as pd
+
+years_list = []
+id_list = []
+numbers_list = []
+
+with open("toto_6x49.txt") as f:
+    for f in f.readlines():
+        year = f.split(",")[0]
+        id = f.split(",")[1]
+        winning_numbers = ",".join(f.split(",")[2:])
+        years_list.append(year)
+        id_list.append(id)
+        numbers_list.append(winning_numbers)
+
+out_dict = {
+    "Year":years_list,
+    "ID":id_list,
+    "Numbers":numbers_list
+}
+df = pd.DataFrame.from_dict(out_dict)
+df.to_excel("6x49.xlsx", index=False)
+df.to_csv("6x49.csv", index=False)
